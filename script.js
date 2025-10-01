8bc69ac3d933ce9be709347386aca2609f2313ad
M4tisPto <matis.pto.104250@gmail.com>
2025-10-01 12:44:24 -0300

    papaas

---
+++ script.js
@@ -0,0 +1,17 @@
+// function multiplicarNumeros(n1, n2){
+//     const resultado = n1 * n2
+//     const esPar = resultado % 2 === 0
+//     if(esPar){
+//         console.log('El resultado de: ' + resultado + ' es par')
+//     } else{
+//         console.log('El resultado de: ' + resultado + ' es impar')
+//     }
+//     return resultado, esPar;
+// }
+
+// console.log(multiplicarNumeros(2, 5))
+
+let n1 = 10;
+n1 = Math.abs(n1);
+console.log('El valor absoluto de este numero es: ' + n1);
+
